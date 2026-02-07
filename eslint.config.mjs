f58f072eaeec4885c4453abe699e456b0fecf72d
DaRiaN0Dev <mohammadramezani.work@gmail.com>
2026-02-08 01:57:02 +0330

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,11 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig } from "@fullstacksjs/eslint-config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default defineConfig({
+  typescript: {
+    tsconfigRootDir: import.meta.dirname,
+  },
+  react: true,
+  next: true,
+  tailwind: true,
+  ignores: ["postcss.config.mjs", "eslint.config.mjs"],
+});
